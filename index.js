15634a869f93eed010712fba4bc0942b95180d57
ksenia-khait <89581440+ksenia-khait@users.noreply.github.com>
2022-09-19 17:35:35 +0300

    animation

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const acc = document.querySelector(".accordion");
-let i;
-
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", () => {
-        this.classList.toggle("active");
-        let panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
-    });
-}
+// const acc = document.querySelector(".accordion");
+// let i;
+//
+// for (i = 0; i < acc.length; i++) {
+//     acc[i].addEventListener("click", () => {
+//         this.classList.toggle("active");
+//         let panel = this.nextElementSibling;
+//         if (panel.style.display === "block") {
+//             panel.style.display = "none";
+//         } else {
+//             panel.style.display = "block";
+//         }
+//     });
+// }
 
 //Анимация
 
